67b9d6666b1b144daf9330bdffec6c2d20f48a8a
Anna Jo <t-anjo@microsoft.com>
2019-07-25 14:30:00 -0700

    launch gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+//in development
 var gulp = require("gulp");
 var webserver = require("gulp-webserver");
 
@@ -6,7 +7,7 @@ var webserver = require("gulp-webserver");
  */
 gulp.task("default", function () {
     var options = {
-        port: 8000,
+        port: 7000,
         livereload: false
     };
 

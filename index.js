a806f4a0b4468058c0e92537368cf073bde9c58b
Anna Jo <t-anjo@microsoft.com>
2019-07-25 11:23:51 -0700

    index.

--- index.js
+++ index.js
@@ -15,20 +15,23 @@ var createScene = function () {
     var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
     // Default intensity is 1. Let's dim the light a small amount
     light.intensity = 0.7;
-    // Our built-in 'sphere' shape.
-    var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 2, segments: 32 }, scene);
+    //0.7 - only 70% of light source in 3d world
+    // Our built-in 'box' shape.
+    var box = BABYLON.MeshBuilder.CreateBox("box", { size: 1 }, scene);
     // Move the sphere upward 1/2 its height
-    sphere.position.y = 1;
-    // Our built-in 'ground' shape.
+    box.position.y = 1;
+    // Our built-in 'ground' shape; at pos.y = 0
     var ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
     return scene;
 };
+//host:
 var canvas = document.getElementById('renderCanvas');
-var engine = new BABYLON.Engine(canvas, true);
+//casted canvas so TS knows this is a canvas element
+var engine = new BABYLON.Engine(canvas, true); //turn on engine
 window.addEventListener('resize', function () {
     engine.resize();
 });
-var scene = createScene(); //where we are; container but NEED camera
+var scene = createScene();
 engine.runRenderLoop(function () {
     scene.render();
 });
--- index.ts
+++ index.ts
@@ -4,7 +4,6 @@
  */
 
 var createScene = function () {
-
     // This creates a basic Babylon Scene object (non-mesh)
     var scene = new BABYLON.Scene(engine);
 
@@ -21,30 +20,33 @@ var createScene = function () {
     var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
 
     // Default intensity is 1. Let's dim the light a small amount
-    light.intensity = 0.7;
+    light.intensity = 0.7; 
+    //0.7 - only 70% of light source in 3d world
 
-    // Our built-in 'sphere' shape.
-    var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
+    // Our built-in 'box' shape.
+    var box = BABYLON.MeshBuilder.CreateBox("box", {size: 1}, scene);
 
     // Move the sphere upward 1/2 its height
-    sphere.position.y = 1;
+    box.position.y = 1;
 
-    // Our built-in 'ground' shape.
+    // Our built-in 'ground' shape; at pos.y = 0
     var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
 
     return scene;
-
 };
 
+//host:
 var canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
-var engine = new BABYLON.Engine(canvas, true);
+//casted canvas so TS knows this is a canvas element
 
-window.addEventListener('resize', () => {   //checks if user resizes window
+var engine = new BABYLON.Engine(canvas, true); //turn on engine
+
+window.addEventListener('resize', () => {   //canvas resizes with window
     engine.resize();
 });
 
-var scene = createScene();  //where we are; container but NEED camera
+var scene = createScene();
 
-engine.runRenderLoop(() => {    //loop that gives new image to system at around 60 fps
+engine.runRenderLoop(() => {    //loop that animates images at 60 fps
     scene.render();
 });

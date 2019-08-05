7c33e3b0128ccd14d9b33e281e10df06f24b126a
RJ Quiban <t-riquib@microsoft.com>
2019-08-05 11:22:34 -0700

    Organization (hopefully)

--- controls.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/*
- *  File for managing keyboard controls
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=controls.js.map
--- controls.ts
+++
@@ -1,6 +0,0 @@
-/*
- *  File for managing keyboard controls
- */
-
-import {createScene} from './index';
-

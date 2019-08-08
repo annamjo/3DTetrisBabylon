a2c91101d57fcca140860074206259f76ea8aef9
Anna Jo <t-anjo@microsoft.com>
2019-08-08 11:12:32 -0700

    GameBoard class

---
+++ 3DTetris/GameBoard.js
@@ -0,0 +1,6 @@
+var GameBoard = /** @class */ (function () {
+    function GameBoard() {
+    }
+    return GameBoard;
+}());
+//# sourceMappingURL=GameBoard.js.map
---
+++ 3DTetris/GameBoard.ts
@@ -0,0 +1,3 @@
+class GameBoard {
+    //plan: create a gameboard class, pass in params to construc to change size
+}

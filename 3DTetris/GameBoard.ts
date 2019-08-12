2ce64e64bbfae139c37b5ebdb53c78bd633f6db0
Anna Jo <t-anjo@microsoft.com>
2019-08-12 09:56:15 -0700

    restructuring gameboard

--- 3DTetris/GameBoard.ts
+++ 3DTetris/GameBoard.ts
@@ -1,19 +1,18 @@
 class GameBoard {
     private _size: number;
+    private _depth: number;
     private _ground: BABYLON.Mesh; //a babylon mesh
-    // cameraCalib: number;
-    private _position: boolean[]; //array of nums //or BABYLON.Vector3, each square of grid - pos in gameboard
-    //number[]??
+    // cameraCalib: number; //dep on size
+    private _positions: boolean[]; //number[] //or BABYLON.Vector3, each square of grid - pos in gameboard
     //2d/3d array
     
     constructor(size: number) {
         this._size = size;
         this.create();
-        this._position = new Array();
     }
 
     private create(): void { //only used within this class
-        var groundGrid = createGrid();
+        var groundGrid = this.createGrid();
         groundGrid.backFaceCulling = false;
     
         //size: must be odd number b/c of offset; use 5 or 7
@@ -23,40 +22,50 @@ class GameBoard {
         this._ground = ground;
 
         //front & back planes
-        var fplane = createPlane(0, 0, -this._size/2, Math.PI);
-        var bplane = createPlane(0, 0, this._size/2, 0);
+        var fplane = this.createPlane(0, 0, -this._size/2, Math.PI);
+        var bplane = this.createPlane(0, 0, this._size/2, 0);
 
         //right & left planes
-        var rplane = createPlane(this._size/2, 0, 0, Math.PI / 2);
-        var lplane = createPlane(-this._size/2, 0, 0, -Math.PI/2);
-
-        function createGrid() { //take out and make private??
-            var grid = new BABYLON.GridMaterial("grid", scene);
-            grid.lineColor = BABYLON.Color3.White();
-            grid.majorUnitFrequency = 1;
-            grid.opacity = 0.8; 
-            grid.gridOffset = new BABYLON.Vector3(0.5, 0, 0.5);
-            return grid;
-        }
-
-        function createPlane(x: number, y: number, z: number, rotation: number) {
-            var height = (this._size === 7) ? 12 : 10; //12 if 7, 10 if 5 (default)
-            var plane = BABYLON.MeshBuilder.CreatePlane("plane", {height: height, width: this._size}, scene);
-            plane.position.x = x;
-            plane.position.y = y; //need??
-            plane.position.z = z;
-            plane.rotation.y = rotation;
-    
-            var planeGrid = createGrid();
-            planeGrid.backFaceCulling = true;
-            plane.material = planeGrid;
-            plane.checkCollisions = true;
-    
-            return plane;
-        }
+        var rplane = this.createPlane(this._size/2, 0, 0, Math.PI / 2);
+        var lplane = this.createPlane(-this._size/2, 0, 0, -Math.PI/2);
     }
 
-    get ground(): BABYLON.Mesh {
+    get positions(): boolean[] { //1 square longer each dim (always set to true - occupied)
+        var width = this._size;
+        var length = this._size;
+        var depth = this._depth;
+        return this._positions;
+    }
+
+    //doesblock fit in? (next block, current block)
+    //collapse layer, is layer full?
+
+    private createGrid(): BABYLON.GridMaterial {
+        var grid = new BABYLON.GridMaterial("grid", scene);
+        grid.lineColor = BABYLON.Color3.White();
+        grid.majorUnitFrequency = 1;
+        grid.opacity = 0.8; 
+        grid.gridOffset = new BABYLON.Vector3(0.5, 0, 0.5);
+        return grid;
+    }
+
+    private createPlane(x: number, y: number, z: number, rotation: number): BABYLON.Mesh {
+        this._depth = (this._size === 7) ? 12 : 10; //12 if 7, 10 if 5 (default)
+        var plane = BABYLON.MeshBuilder.CreatePlane("plane", {height: this._depth, width: this._size}, scene);
+        plane.position.x = x;
+        plane.position.y = y; //need??
+        plane.position.z = z;
+        plane.rotation.y = rotation;
+
+        var planeGrid = this.createGrid();
+        planeGrid.backFaceCulling = true;
+        plane.material = planeGrid;
+        plane.checkCollisions = true;
+
+        return plane;
+    }
+
+    public get ground(): BABYLON.Mesh {
         return this._ground;
     }
 }

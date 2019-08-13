283f81d3a0a611dc8864b2099b1f4f77b9368fcd
Anna Jo <t-anjo@microsoft.com>
2019-08-13 15:32:06 -0700

    Game class updated with scene

--- 3DTetris/Game.ts
+++ 3DTetris/Game.ts
@@ -1,21 +1,54 @@
 /*import {GameBoard} from './GameBoard.js';*/
 
 class Game {
-    // private _scene: BABYLON.Scene;
+    private _scene: BABYLON.Scene;
+    private _canvas: HTMLCanvasElement;
+    private _camera: BABYLON.ArcRotateCamera;
 
     constructor() {
         // create canvas, scene (has gameboard), engine?
+        this._canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
+        const engine = new BABYLON.Engine(this._canvas, true);
+        this._scene = new BABYLON.Scene(engine);
+
+        window.addEventListener("resize", () => {
+            engine.resize();
+        });
+        
+        this.createScene();
+
+        engine.runRenderLoop(() => { 
+            this._scene.render();
+        });
     }
 
-    // createScene() {} //animation loop
+    private createScene() {
+        const engine = this._scene.getEngine();
+        //var scene = new BABYLON.Scene(engine);
+    
+        var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI/2, Math.PI/3.3, 18.4, new BABYLON.Vector3(0, 0, 0), this._scene); //camera changed
+        camera.attachControl(this._canvas, true);
 
-    drawBlock() { //loop - draw another block based on time interval/when current block not active
+        var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), this._scene);
+        light.intensity = 1;
+
+        var box = BABYLON.MeshBuilder.CreateBox("box", {size: 1}, this._scene);
+        box.position.y = 5.5;
+    }
+
+    private drawBlock() { //loop - draw another block based on time interval/when current block not active
         //randomize block - array of options (string or number); spawn a random block
     } //draw drop preview?
     
-    getNextBlock() {} //also preview next block
+    private getNextBlock() {} //also preview next block
 
-    removeBlocks() {} //when layer cleared
+    private removeBlocks() {} //when layer cleared //is there an ActiveBlock? - check layer after a block locks into place (no active blocks)
 
-    collpaseBlocks() {} //when gameboard layers (spaces array) collapse
+    private collpaseBlocks() {
+        let layerObserver = this._scene.onAfterRenderObservable.add( () => {
+
+        });
+    } //when gameboard layers (spaces array) collapse
 }
+
+new Game();

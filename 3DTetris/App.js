c74e66ad8ee0c448ebcccf90d4ec07a46546289d
Anna Jo <t-anjo@microsoft.com>
2019-08-29 14:39:55 -0700

    App w/Menu, Game, and GameOver loop

--- 3DTetris/App.js
+++ 3DTetris/App.js
@@ -1,4 +1,3 @@
-// import {Menu} from './Menu.js';
 var App = /** @class */ (function () {
     function App() {
         var _this = this;
@@ -10,21 +9,71 @@ var App = /** @class */ (function () {
         window.addEventListener("resize", function () {
             engine.resize();
         });
-        this.createScene();
+        this._gameOver = false;
+        this._score = 0;
+        this.createScene(this._scene);
         engine.runRenderLoop(function () {
-            _this._scene.render();
+            // this._scene.render();
+            if (!_this._gameOver) {
+                _this._scene.render();
+            }
+            else if (_this._gameOver) {
+                _this._menu._advancedTexture.dispose();
+                _this._scene.dispose();
+                var scene2 = new BABYLON.Scene(engine);
+                _this.createScene(scene2);
+                _this._scene = scene2; //need?
+                scene2.render();
+            }
         });
     }
-    App.prototype.createScene = function () {
-        var engine = this._scene.getEngine();
-        //var scene = new BABYLON.Scene(engine);
-        // scene.clearColor = new BABYLON.Color4(0, 0, 0, 0);
-        var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2, Math.PI / 3.3, 18.4, new BABYLON.Vector3(0, 0, 0), this._scene); //camera changed
+    App.prototype.createScene = function (scene) {
+        var _this = this;
+        var engine = scene.getEngine();
+        var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2, Math.PI / 3.3, 18.4, new BABYLON.Vector3(0, 0, 0), scene); //camera changed
         camera.attachControl(this._canvas, true);
-        var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), this._scene);
+        var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
         light.intensity = 1;
-        // var menu = new Menu(this._scene);
-        var game = new Game(7, this._scene);
+        if (this._gameOver) {
+            scene.clearColor = new BABYLON.Color4(0, 0, 0, 0);
+            this._endScreen = new GameOver(scene, this._score);
+            var pointerDown_1 = scene.onPointerObservable.add(function (pointerInfo) {
+                switch (pointerInfo.type) {
+                    case BABYLON.PointerEventTypes.POINTERDOWN:
+                        scene.onPointerObservable.remove(pointerDown_1);
+                        _this._endScreen.active = false;
+                        _this._endScreen.hide();
+                        _this._gameOver = false;
+                        _this._game = new Game(7, scene);
+                        scene.registerBeforeRender(function () {
+                            if (_this._game.gameOver) {
+                                _this._gameOver = true;
+                                _this._score = _this._game.scoreCount;
+                            }
+                        });
+                        break;
+                }
+            });
+        }
+        else {
+            this._menu = new Menu(scene);
+            var pointerDown_2 = scene.onPointerObservable.add(function (pointerInfo) {
+                switch (pointerInfo.type) {
+                    case BABYLON.PointerEventTypes.POINTERDOWN:
+                        scene.onPointerObservable.remove(pointerDown_2);
+                        _this._menu.active = false;
+                        _this._menu.hide();
+                        _this._game = new Game(7, scene);
+                        scene.registerBeforeRender(function () {
+                            if (_this._game.gameOver) {
+                                _this._gameOver = true;
+                                _this._score = _this._game.scoreCount;
+                            }
+                        });
+                        break;
+                }
+            });
+        }
     };
     return App;
 }());
--- 3DTetris/App.ts
+++ 3DTetris/App.ts
@@ -1,8 +1,11 @@
-// import {Menu} from './Menu.js';
-
 class App {
     private _scene: BABYLON.Scene;
     private _canvas: HTMLCanvasElement;
+    private _menu: Menu;
+    private _game: Game;
+    private _gameOver: boolean;
+    private _endScreen: GameOver;
+    private _score: number;
 
     constructor() {
         // create canvas, scene (has gameboard), engine?
@@ -15,27 +18,76 @@ class App {
             engine.resize();
         });
         
-        this.createScene();
+        this._gameOver = false;
+        this._score = 0;
+        this.createScene(this._scene);
 
         engine.runRenderLoop(() => { 
-            this._scene.render();
+            // this._scene.render();
+            if (!this._gameOver) {
+                this._scene.render();
+            }
+            else if (this._gameOver) {
+                this._menu._advancedTexture.dispose();
+                this._scene.dispose();
+                var scene2 = new BABYLON.Scene(engine);
+                this.createScene(scene2);
+                this._scene = scene2; //need?
+                scene2.render();
+            }
         });
     }
 
-    private createScene() {
-        const engine = this._scene.getEngine();
-        //var scene = new BABYLON.Scene(engine);
-        // scene.clearColor = new BABYLON.Color4(0, 0, 0, 0);
+    private createScene(scene: BABYLON.Scene) {
+        const engine = scene.getEngine();
     
-        var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI/2, Math.PI/3.3, 18.4, new BABYLON.Vector3(0, 0, 0), this._scene); //camera changed
+        var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI/2, Math.PI/3.3, 18.4, new BABYLON.Vector3(0, 0, 0), scene); //camera changed
         camera.attachControl(this._canvas, true);
 
-        var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), this._scene);
+        var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
         light.intensity = 1;
 
-        // var menu = new Menu(this._scene);
-        var game = new Game(7, this._scene);
+        if (this._gameOver) {
+            scene.clearColor = new BABYLON.Color4(0, 0, 0, 0);
+            this._endScreen = new GameOver(scene, this._score);
+            let pointerDown = scene.onPointerObservable.add((pointerInfo) => {
+                switch (pointerInfo.type) {
+                    case BABYLON.PointerEventTypes.POINTERDOWN:
+                        scene.onPointerObservable.remove(pointerDown);
+                        this._endScreen.active = false;
+                        this._endScreen.hide();
+                        this._gameOver = false;
+                        this._game = new Game(7, scene);
+                        scene.registerBeforeRender(() => {
+                            if (this._game.gameOver) {
+                                this._gameOver = true;
+                                this._score = this._game.scoreCount;
+                            }
+                        });
+                        break;
+                }
+            });
+        }
+        else {
+            this._menu = new Menu(scene);
 
+            let pointerDown = scene.onPointerObservable.add((pointerInfo) => {
+                switch (pointerInfo.type) {
+                    case BABYLON.PointerEventTypes.POINTERDOWN:
+                        scene.onPointerObservable.remove(pointerDown);
+                        this._menu.active = false;
+                        this._menu.hide();
+                        this._game = new Game(7, scene);
+                        scene.registerBeforeRender(() => {
+                            if (this._game.gameOver) {
+                                this._gameOver = true;
+                                this._score = this._game.scoreCount;
+                            }
+                        });
+                        break;
+                }
+            });
+        }
     }
 }
 
